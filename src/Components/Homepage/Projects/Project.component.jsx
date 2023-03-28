85aa51679535e775013586eb2cec9b66088c150e
Pranay <pranaydwivedi32@gmail.com>
2023-03-29 00:46:53 +0530

    Done completed project section

--- src/Components/Homepage/Projects/Project.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Project.styles.css";
-
-function Project() {
-  return <div className="project__container ">Project</div>;
-}
-
-export default Project;
--- src/Routes/Home/Home.component.jsx
+++ src/Routes/Home/Home.component.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import Contact from "../../Components/Contact/Contact.component";
 import Experience from "../../Components/Homepage/Experience/Experience.component";
 import Hero from "../../Components/Homepage/Hero/Hero.component";
-import Project from "../../Components/Homepage/Projects/Project.component";
+import Project from "../../Components/Homepage/ProjectsSection/ProjectSection.component";
 import Summary from "../../Components/Homepage/Summary/Summary.component";
 import "./Home.styles.css";
 
